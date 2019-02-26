fa97e832214eb359f5dbef17c869b05ad2066f50
EC2 Default User <ec2-user@ip-172-31-19-7.us-east-2.compute.internal>
2019-02-26 15:35:44 +0000

    RubyBasics3 - Part 1

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -3,12 +3,8 @@
 class BookInStock
     attr_accessor :isbn, :price
     def initialize isbn, price
-        raise ArgumentError if isbn.empty? || price <= 0
-        @isbn = isbn
-        @price = price
-    end
-    def price_as_string
-        format("$%.2f", @price)
+       @isbn = isbn
+       @price = price
     end
 # YOUR CODE HERE
 end
