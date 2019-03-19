f30038ff5a911926aa3463034e924f44afdde107
EC2 Default User <ec2-user@ip-172-31-19-7.us-east-2.compute.internal>
2019-03-19 22:26:59 +0000

    Add error messages for title validations

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
     def new
+        @article = Article.new
     end
     def index
         @articles = Article.all
@@ -10,10 +11,11 @@ class ArticlesController < ApplicationController
     def create
        # render plain: params[:article].inspect
        @article = Article.new(article_params)
-       
-       @article.save
-       redirect_to @article
-    
+       if @article.save
+           redirect_to @article
+       else
+            render 'new'
+       end
     end
 end
 
