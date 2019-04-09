bc58f2da9bdcf205eb333ec0bc80d3b27b68f749
EC2 Default User <ec2-user@ip-172-31-19-7.us-east-2.compute.internal>
2019-04-09 14:28:07 +0000

    Install and Set Up Cucumber

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -6,7 +6,7 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
     def show
-        @articles = Article.find(params[:id])
+        @article = Article.find(params[:id])
     end
     def create
        # render plain: params[:article].inspect
